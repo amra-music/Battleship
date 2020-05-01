ed1106f67d7ebba890c40123db68817c3682b1be
Amra <amusic5@etf.unsa.ba>
2020-05-01 15:25:11 +0200

    Prije promjena listenera

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,19 +1,12 @@
 package sample;
 
-import javafx.beans.property.Property;
 import javafx.beans.property.SimpleDoubleProperty;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.event.Event;
-import javafx.event.EventHandler;
-import javafx.event.EventType;
 import javafx.fxml.FXML;
 import javafx.geometry.Bounds;
 import javafx.scene.Cursor;
 import javafx.scene.Node;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
-import javafx.scene.input.DragEvent;
 import javafx.scene.input.MouseDragEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
@@ -24,11 +17,9 @@ import javafx.scene.paint.ImagePattern;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
-import javafx.scene.transform.Transform;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 
 public class Controller {
@@ -38,7 +29,6 @@ public class Controller {
     public Rectangle boatDestroyer;
     public Rectangle boatBattleship;
     public GridPane playerBoard;
-    public Pane ships;
     public Rectangle field;
     public TextField text;
     public Rectangle field1;
@@ -53,12 +43,6 @@ public class Controller {
     private Image boatTwoImage = new Image("sample/boatTwo.png");
     private Image ocean = new Image("sample/ocean.jpg");
     public Pane scene2;
-    public VBox vbox;
-    private boolean clicked;
-    private boolean needToRotate;
-    private boolean isRotated;
-    private Rectangle selectedShip;
-    private double cellSize = 30;
     private double orgSceneX;
     private double orgSceneY;
     private double orgTranslateX;
@@ -81,29 +65,12 @@ public class Controller {
                 nodes.add((Shape) currentNode);
             }
         }
-        /*nodes.add(field);
-        nodes.add(field1);
-        nodes.add(field2);
-        nodes.add(field3);
-        nodes.add(field5);
-        nodes.add(field4);*/
 
         boatCarrier.setFill(new ImagePattern(boatFiveImage));
         boatBattleship.setFill(new ImagePattern(boatFourImage));
         boatCruiser.setFill(new ImagePattern(boatThree2Image));
         boatSubmarine.setFill(new ImagePattern(boatThree1Image));
         boatDestroyer.setFill(new ImagePattern(boatTwoImage));
-        playerBoard.setStyle("-fx-border-color: #ffdc32");
-
-        boundsInScene = scene2.localToScene(scene2.getBoundsInLocal());
-
-        ploca = playerBoard.localToScene(playerBoard.getBoundsInLocal());
-
-
-        boja = field.getFill();
-
-
-        text.textProperty().bind(boatCarrier.boundsInLocalProperty().asString());
 
     }
 
@@ -128,8 +95,6 @@ public class Controller {
         if (mouseEvent.isSecondaryButtonDown()) {
             setRotation(boatCarrier);
         }
-        System.out.println(boatCarrierX.get());
-
     }
 
     public void playerBoardDragDetected(MouseEvent mouseEvent) {
@@ -146,10 +111,8 @@ public class Controller {
             ((Rectangle) (mouseEvent.getSource())).setTranslateX(orgTranslateX);
             ((Rectangle) (mouseEvent.getSource())).setTranslateY(orgTranslateY);
         }
-
         //boatCarrier.setMouseTransparent(false);
         playerBoard.toBack();
-
     }
 
     public void boatCarrierMouseDragged(MouseEvent mouseEvent) {
@@ -162,12 +125,7 @@ public class Controller {
         boatCarrier.setTranslateX(newTranslateX);
         boatCarrier.setTranslateY(newTranslateY);
 
-        Bounds brodic = boatCarrier.localToScene(boatCarrier.getBoundsInParent());
-
-        Bounds polje = field.localToScene(field.getBoundsInLocal());
-        //if(boatCarrier.intersects(polje))field.setFill(Color.RED);
         checkShapeIntersection(boatCarrier);
-
     }
 
     private void setRotation(Rectangle ship) {
